28d6146f8879e1fadeb1404870ab3288ecf0d516
webfarrock <webfarrock@mail.ru>
2022-02-13 11:01:02 +0300

    basic project setup

---
+++ src/js/index.ts
@@ -0,0 +1,3 @@
+import "../styles/style.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+console.log("test out");
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+const { resolve } = require("path");
+
+const { NODE_ENV } = process.env;
+
+module.exports = {
+  entry: resolve(__dirname, "src/js/index.ts"),
+  resolve: {
+    extensions: [".js", ".ts"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: resolve(`${__dirname}/dist`),
+    clean: true,
+    environment: {
+      arrowFunction: false,
+    },
+  },
+  devtool: NODE_ENV === "production" ? "hidden-source-map" : "eval-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    ],
+  },
+  mode: NODE_ENV === "production" ? "production" : "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: resolve(__dirname, "index.html"),
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  optimization: {
+    minimizer: [`...`, new CssMinimizerPlugin()],
+  },
+  devServer: {
+    compress: true,
+    port: 9000,
+    client: {
+      logging: "info",
+    },
+  },
+};
