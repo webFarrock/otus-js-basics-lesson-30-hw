f453986c658af7791225c72293bcd439ace4b388
webfarrock <webfarrock@mail.ru>
2022-02-24 09:28:31 +0300

    apply prettier fix

--- src/js/services/city.ts
+++ src/js/services/city.ts
@@ -3,13 +3,13 @@ import axios from "axios";
 const GEO_JS_GET_PATH = "https://get.geojs.io/v1/ip/geo.json";
 
 const getCurrentCity = async (): Promise<string | null> => {
-    try {
-        const res = await axios.get(GEO_JS_GET_PATH);
-        return res.data.city;
-    } catch (e) {
-        console.error(`error while get geo js data: `, e);
-        return null;
-    }
+  try {
+    const res = await axios.get(GEO_JS_GET_PATH);
+    return res.data.city;
+  } catch (e) {
+    console.error(`error while get geo js data: `, e);
+    return null;
+  }
 };
 
-export {getCurrentCity, GEO_JS_GET_PATH};
+export { getCurrentCity, GEO_JS_GET_PATH };
